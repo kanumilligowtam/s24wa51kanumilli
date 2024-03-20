11f265d9d50343cebed0f6d59db2cca844709b5e
kanumilligowtam <kanumilligowtham@gmail.com>
2024-03-20 16:20:00 -0500

    initial commit

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+var product = 1;
+var product = 1;
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  factor++;
+  product *= factor;
+  res.send(`Product is: ${product}`);
 });
 
 module.exports = router;
